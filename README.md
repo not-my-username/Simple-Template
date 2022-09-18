5c55b29bccee9f7bc7edee58e183c21b7db2229f
Liam <something.random.i.made.at.3am@gmail.com>
2022-09-19 09:52:48 +1200

    inti

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Template
+This is the template I used on some my other projects
---
+++ index.js
@@ -0,0 +1,18 @@
+state = 0
+
+//Define functions here:
+
+$(document).ready(function() {
+    // Program logic:
+    $("#mainButton").on("click", function() {
+        switch (state) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    });
+    //Other on click logic:
+});
